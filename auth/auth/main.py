2e5c509e27c9dce19e8734638b2ea73b477a0859
Miguel Silva <msilvaale08@gmail.com>
2020-12-16 19:31:49 -0500

    docker setup

--- auth/auth/main.py
+++ auth/auth/main.py
@@ -1,17 +1,11 @@
-import uvicorn
-
 from fastapi import FastAPI
 from auth.routers import users
 
 app = FastAPI()
-
 subapi = FastAPI(openapi_prefix="/auth/v1")
 app.mount("/auth/v1", subapi)
 
-general_responses = {404: {"description": "Not found"}}
 
-def main():
-    subapi.include_router(users.router)
+subapi.include_router(users.router)
 
-if __name__ == "__main__":
-    main()
+
